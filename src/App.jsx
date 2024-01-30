4a3593565aeea1b107f85a8c24cb1d9bea00a019
suvadip717 <suvadipdas717@gmail.com>
2024-01-30 12:21:23 +0530

    about complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import Navbar from "./components/Navbar"
 import Home from "./components/Home"
 import SocialLinks from "./components/SocialLinks"
+import About from "./components/About"
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Navbar/>
       <Home/>
       <SocialLinks/>
+      <About/>
     </>
   )
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div
+    name = "about"
+    className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'
+    >
+        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
+            </div>
+            <p className='text-xl mt-16'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam animi architecto, quod nulla dignissimos nesciunt tempora assumenda tempore excepturi numquam voluptate. Velit ex deserunt commodi qui itaque veritatis laborum et similique harum error? Laboriosam tenetur modi corporis nam perferendis culpa commodi? Repudiandae blanditiis ab commodi vel odit. Aliquam, recusandae explicabo. Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus voluptate vitae placeat quia commodi, sed aut distinctio libero vel hic.</p>
+            <br />
+            <p className='text-xl mt-8'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, aliquid. Nulla ratione, quaerat impedit, animi exercitationem fuga deleniti optio odit quis doloribus ipsam, explicabo delectus. Sapiente laboriosam id, accusamus aspernatur magni aut consequuntur esse sunt corporis vel ipsam iste a debitis impedit voluptatem hic quidem illo suscipit laborum ut consectetur illum earum soluta facilis! Ea obcaecati repudiandae, distinctio deleniti culpa eum facere animi architecto error incidunt eveniet earum necessitatibus veniam!</p>
+        </div>
+    </div>
+  )
+}
+
+export default About
